c341723841816707a455ee0162a7304891510122
richwill28 <e0550368@u.nus.edu>
2022-01-15 02:09:55 +0800

    Minor UI tweak

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ export default function Header({ grid, startNode, endNode, isDarkState }) {
       </div>
       <div className="flex flex-row items-center gap-6">
         <button
-          className="bg-sky-400 hover:bg-sky-500 text-[15px] font-mono font-bold rounded px-2.5 py-1 shadow-md shadow-sky-900/50 active:shadow-sky-900/30"
+          className="bg-sky-400 hover:bg-sky-500 text-[15px] font-mono font-bold rounded px-2.5 py-1 shadow-md shadow-sky-900/50 active:shadow-sky-900/30 dark:shadow-sky-400/50 dark:active:shadow-sky-400/30"
           onClick={visualize}
         >
           VISUALIZE
--- src/utils/constants/style.js
+++ src/utils/constants/style.js
@@ -4,5 +4,5 @@ export const STYLE_VISITED = STYLE_UNVISITED + " bg-cyan-400";
 export const STYLE_START = STYLE_UNVISITED + " bg-emerald-400";
 export const STYLE_END = STYLE_UNVISITED + " bg-rose-400";
 export const STYLE_WALL_DARK = STYLE_UNVISITED + " bg-gray-700";
-export const STYLE_WALL_LIGHT = STYLE_UNVISITED + " bg-gray-200";
+export const STYLE_WALL_LIGHT = STYLE_UNVISITED + " bg-gray-100";
 export const STYLE_PATH = STYLE_UNVISITED + " bg-amber-200";
