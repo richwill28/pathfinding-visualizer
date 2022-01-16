cf757df41d8d689c61421f2bef48624b231087a1
richwill28 <e0550368@u.nus.edu>
2022-01-17 04:44:43 +0800

    Update dijkstra implementation

--- src/utils/algorithms/graph/dijkstra.js
+++ src/utils/algorithms/graph/dijkstra.js
@@ -1,24 +1,38 @@
 import { MAX_ROW, MAX_COL } from "../../constants/variable";
 
-// Inefficient implementation without using priority queue
-export default function dijkstra(grid, startNode, endNode) {
+// non-optimized implementation without using proper priority queue data structure
+export default function bfs(grid, startNode, endNode) {
   const visitedNodes = [];
 
   // Dijkstra main routine
   grid[startNode.row][startNode.col].distance = 0;
-  const unvisitedNodes = getNodes(grid);
+  const unvisitedNodes = [grid[startNode.row][startNode.col]];
   while (unvisitedNodes.length > 0) {
-    unvisitedNodes.sort((a, b) => a.distance - b.distance);
+    unvisitedNodes.sort((a, b) => a.distance - b.distance); // node with smaller distance has higher priority
     const node = unvisitedNodes.shift();
     if (node.isWall) continue; // skip wall
     if (node.distance === Infinity) break; // trapped
     node.isVisited = true;
     visitedNodes.push(node);
     if (node.row === endNode.row && node.col === endNode.col) break; // finish
+
+    // traverse neighbors
     const neighbors = getUnvisitedNeighbors(grid, node);
     for (const neighbor of neighbors) {
-      neighbor.distance = node.distance + 1;
-      neighbor.parent = node;
+      let isSkip = false; // prevent pushing the same node into the priority queue more than once
+      for (const unvisitedNode of unvisitedNodes) {
+        if (
+          neighbor.row === unvisitedNode.row &&
+          neighbor.col === unvisitedNode.col
+        ) {
+          isSkip = true;
+        }
+      }
+      if (!isSkip) {
+        neighbor.distance = node.distance + 1;
+        neighbor.parent = node;
+        unvisitedNodes.push(neighbor);
+      }
     }
   }
 
@@ -34,22 +48,12 @@ export default function dijkstra(grid, startNode, endNode) {
   return { visitedNodes, shortestPath };
 }
 
-function getNodes(grid) {
-  const nodes = [];
-  for (const row of grid) {
-    for (const node of row) {
-      nodes.push(node);
-    }
-  }
-  return nodes;
-}
-
 function getUnvisitedNeighbors(grid, node) {
   const { row, col } = node;
   const neighbors = [];
-  if (row > 0) neighbors.push(grid[row - 1][col]);
-  if (row < MAX_ROW - 1) neighbors.push(grid[row + 1][col]);
-  if (col > 0) neighbors.push(grid[row][col - 1]);
-  if (col < MAX_COL - 1) neighbors.push(grid[row][col + 1]);
+  if (col > 0) neighbors.push(grid[row][col - 1]); // left
+  if (row > 0) neighbors.push(grid[row - 1][col]); // top
+  if (col < MAX_COL - 1) neighbors.push(grid[row][col + 1]); // right
+  if (row < MAX_ROW - 1) neighbors.push(grid[row + 1][col]); // bottom
   return neighbors.filter((neighbor) => !neighbor.isVisited);
 }
